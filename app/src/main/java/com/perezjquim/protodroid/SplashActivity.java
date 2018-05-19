277082e463ff8dc3b6a5e59598588562e15dedcf
Joaquim Perez <jquim1@hotmail.com>
2018-05-20 00:00:17 +0100

    The beginning of them all

---
+++ app/src/main/java/com/perezjquim/protodroid/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.perezjquim.protodroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        // Passa para a atividade principal (MainActivity)
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
